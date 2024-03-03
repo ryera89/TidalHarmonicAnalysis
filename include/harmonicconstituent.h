1cf66ccdab9c5042754c230f1d33963efbae50a3
Rolando Yera Moreno <aceryera89@gmail.com>
2024-03-03 14:12:18 -0300

    Added HarmonicConstants to HarmonicConstituents class

--- include/harmonicconstituent.h
+++ include/harmonicconstituent.h
@@ -7,13 +7,19 @@
 
 namespace HarmonicAnalysis {
 
-struct TidalHarmonicAnalysis_EXPORT NodalCorrections {
-    double amplitude;
-    double angle;
-};
-
 class TidalHarmonicAnalysis_EXPORT HarmonicConstituent {
   public:
+    struct NodalCorrections {
+        double amplitude;
+        double angle;
+    };
+
+    struct HarmonicConstants {
+        // Harmonic constants must be computed from sea level measurements
+        double amplitude = 0.0;
+        double phase     = 0.0;
+    };
+
     explicit HarmonicConstituent(
         double frequency,
         const std::array<int, 7>& extended_doodson_numbers,
@@ -21,6 +27,7 @@ class TidalHarmonicAnalysis_EXPORT HarmonicConstituent {
         return NodalCorrections{.amplitude = 1.0, .angle = 0.0};
     }
     );
+
     explicit HarmonicConstituent(
         double frequency,
         std::array<int, 7>&& extended_doodson_numbers,
@@ -29,13 +36,34 @@ class TidalHarmonicAnalysis_EXPORT HarmonicConstituent {
     }
     );
 
+    explicit HarmonicConstituent(
+        double frequency,
+        HarmonicConstants harmonicConstants,
+        const std::array<int, 7>& extended_doodson_numbers,
+        std::function<NodalCorrections(double, double)> func = [](double, double) -> NodalCorrections {
+        return NodalCorrections{.amplitude = 1.0, .angle = 0.0};
+    }
+    );
+
+    explicit HarmonicConstituent(
+        double frequency,
+        HarmonicConstants harmonicConstants,
+        std::array<int, 7>&& extended_doodson_numbers,
+        std::function<NodalCorrections(double, double)> func = [](double, double) -> NodalCorrections {
+        return NodalCorrections{.amplitude = 1.0, .angle = 0.0};
+    }
+    );
+
+    ~HarmonicConstituent();
+
     double frequency() const;
     const std::array<int, 7>& extendedDoodsonNumbers() const;
     NodalCorrections nodalCorrections(double perigeeLongitude, double ascendingNodeLongitude) const;
+    HarmonicConstants harmonicConstants();
 
   private:
     struct HarmonicConstituentImpl;
-    std::unique_ptr<HarmonicConstituentImpl> m_p;
+    std::unique_ptr<HarmonicConstituentImpl> m_impl;
 };
 
 } // namespace HarmonicAnalysis
--- src/harmonicconstituent.cpp
+++ src/harmonicconstituent.cpp
@@ -22,8 +22,30 @@ struct HarmonicConstituent::HarmonicConstituentImpl {
     : m_frequency{frequency}
     , m_extended_doodson_numbers{extended_doodson_numbers}
     , m_nodal_corrections_func{func} {};
+    explicit HarmonicConstituentImpl(
+        double frequency,
+        HarmonicConstants harmonicConstants,
+        const std::array<int, 7>& extended_doodson_numbers,
+        std::function<NodalCorrections(double, double)> func
+    )
+    : m_frequency{frequency}
+    , m_harmonic_constants{harmonicConstants}
+    , m_extended_doodson_numbers{extended_doodson_numbers}
+    , m_nodal_corrections_func{func} {};
+
+    explicit HarmonicConstituentImpl(
+        double frequency,
+        HarmonicConstants harmonicConstants,
+        std::array<int, 7>&& extended_doodson_numbers,
+        std::function<NodalCorrections(double, double)> func
+    )
+    : m_frequency{frequency}
+    , m_harmonic_constants{harmonicConstants}
+    , m_extended_doodson_numbers{extended_doodson_numbers}
+    , m_nodal_corrections_func{func} {};
 
     double m_frequency;
+    HarmonicConstants m_harmonic_constants;
     std::array<int, 7> m_extended_doodson_numbers;
     std::function<NodalCorrections(double, double)> m_nodal_corrections_func;
 };
@@ -42,14 +64,33 @@ HarmonicConstituent::HarmonicConstituent(
 )
 : m_impl{std::make_unique<HarmonicConstituentImpl>(frequency, extended_doodson_numbers, func)} { }
 
+HarmonicConstituent::HarmonicConstituent(
+    double frequency,
+    HarmonicConstants harmonicConstants,
+    const std::array<int, 7>& extended_doodson_numbers,
+    std::function<NodalCorrections(double, double)> func
+) { }
+
+HarmonicConstituent::HarmonicConstituent(
+    double frequency,
+    HarmonicConstants harmonicConstants,
+    std::array<int, 7>&& extended_doodson_numbers,
+    std::function<NodalCorrections(double, double)> func
+) { }
+
+HarmonicConstituent::~HarmonicConstituent() = default;
+
 double HarmonicConstituent::frequency() const { return m_impl->m_frequency; }
 
 const std::array<int, 7>& HarmonicConstituent::extendedDoodsonNumbers() const {
     return m_impl->m_extended_doodson_numbers;
 }
 
-NodalCorrections HarmonicConstituent::nodalCorrections(double perigeeLongitude, double ascendingNodeLongitude) const {
+HarmonicConstituent::NodalCorrections
+HarmonicConstituent::nodalCorrections(double perigeeLongitude, double ascendingNodeLongitude) const {
     return m_impl->m_nodal_corrections_func(perigeeLongitude, ascendingNodeLongitude);
 }
+
+HarmonicConstituent::HarmonicConstants HarmonicConstituent::harmonicConstants() { return m_impl->m_harmonic_constants; }
 
 } // namespace HarmonicAnalysis
